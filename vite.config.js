9499579ef704a2e1bc693dda8552812f0b29d504
emmett hoolahan <gerhoolahan@icloud.com>
2024-12-22 11:16:47 +0800

    adssda
    adsdsa

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
 // vite.config.js
 import { sveltekit } from '@sveltejs/kit/vite';
-import { ssp } from "sveltekit-search-params/plugin";
+import { defineConfig } from 'vite';
 
-/** @type {import('vite').UserConfig} */
-const config = {
-	plugins: [ssp(), sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	},
-	server: {
-		fs: {
-			// https://vitejs.dev/config/server-options.html#server-fs-allow
-			// allows importing readme for About page
-			allow: ['..']
-		}
-	}
-};
-
-export default config;
+export default defineConfig({
+	plugins: [sveltekit()]
+});
